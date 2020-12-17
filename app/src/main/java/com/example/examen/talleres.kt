66f9f625d32dd6538029f563c05a830fa5e00eaf
Carrasco Romel <romelx23@gmail.com>
2020-12-17 15:49:50 -0500

    Parte1

---
+++ app/src/main/java/com/example/examen/talleres.kt
@@ -0,0 +1,11 @@
+package com.example.examen
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class talleres : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_talleres)
+    }
+}
